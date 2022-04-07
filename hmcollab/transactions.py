a29f024113c7bc13360c34c9b12ef1499d768f8d
Georgina Gonzalez <ginaglezi2000@gmail.com>
2022-04-06 18:04:23 -0700

    kmeans_consumer updated to use attribute customer_dummies as input

--- hmcollab/transactions.py
+++ hmcollab/transactions.py
@@ -16,13 +16,10 @@ class TransactionsByCustomer:
         )
 
 
-def kmeans_consumer(customer, transactions_df, full_articles_dummy, k=1):
+def kmeans_consumer(customer_dummies, k=1):
     # Note: Do not scale dummy features
-    basket = TransactionsByCustomer(transactions_df).all_article_ids(customer)
-    customer_dummies = full_articles_dummy.merge(
-        basket, on="article_id", how="right"
-    ).drop(columns="article_id")
     kmeans = KMeans(
-        init="k-means++", n_clusters=k, n_init=10, max_iter=300, random_state=42
+        init="k-means++", n_clusters=k,
+        n_init=10, max_iter=300, random_state=42
     )
     return kmeans.fit(customer_dummies)
