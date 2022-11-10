0a8abfdbca964882cf9cc36295ff7d1002016584
Reuben Brasher <rkbrasher@gmail.com>
2022-11-09 16:01:32 -0800

    Verify that knn works with split by customer

--- hmcollab/models.py
+++ hmcollab/models.py
@@ -43,23 +43,28 @@ class ArticleKNN:
         return self.model.kneighbors(row)
 
 
-
-
 def filter_articles(trans_df, threshold=50):
     # Compute the number of transactions by article_id. A good threshold for toy is 50, and 300 for full dataset
     # returns:
     #    filtered_article_id: list
-    freqs_df = trans_df.groupby(by=['article_id'], as_index=False).size()
+    freqs_df = trans_df.groupby(by=["article_id"], as_index=False).size()
     freqs_df.reset_index(inplace=True)
-    freqs_df.drop(columns=['index'], inplace=True)
-    freqs_df.rename(columns={'size':'transactions'}, inplace=True)
-    filtered_article_id = freqs_df.loc[freqs_df.transactions>threshold, 'article_id'].to_list()
+    freqs_df.drop(columns=["index"], inplace=True)
+    freqs_df.rename(columns={"size": "transactions"}, inplace=True)
+    filtered_article_id = freqs_df.loc[
+        freqs_df.transactions > threshold, "article_id"
+    ].to_list()
     return filtered_article_id
 
 
 class KnnRecommender:
     def __init__(
-        self, dataset, full_article_dummies, groups=6, total_recommendations=12, threshold=50
+        self,
+        dataset,
+        full_article_dummies,
+        groups=6,
+        total_recommendations=12,
+        threshold=50,
     ):
         self.dataset = dataset
         self.full_article_dummies = full_article_dummies
@@ -68,29 +73,32 @@ class KnnRecommender:
         self.t = transactions.TransactionsByCustomer(self.dataset.train_x)
         filtered_art_ids = filter_articles(self.dataset.train_x, threshold=threshold)
         # I should select features here instead of full
-        self.filtered_dummies = full_article_dummies[full_article_dummies.article_id.isin(
-            filtered_art_ids)]
+        self.filtered_dummies = full_article_dummies[
+            full_article_dummies.article_id.isin(filtered_art_ids)
+        ]
         self.recomendations_by_group = math.ceil(
             self.total_recommendations / self.groups
         )
-        self.model = ArticleKNN(
-            self.filtered_dummies, k=self.recomendations_by_group
-        )
+        self.model = ArticleKNN(self.filtered_dummies, k=self.recomendations_by_group)
 
     def recommend(self, customer, drop_duplicates=True):
         recomendation_ids = []
         # We need full dummies for kmeans
         customer_dummies = self.t.customer_dummies(customer, self.full_article_dummies)
         if drop_duplicates:
-            customer_dummies = customer_dummies.drop_duplicates()     # no article_id
+            customer_dummies = customer_dummies.drop_duplicates()  # no article_id
         min_k = self.groups
         if customer_dummies.shape[0] < self.groups:
-            min_k = customer_dummies.shape[0]   # so far it will produce less recommendations for this customer
+            min_k = customer_dummies.shape[
+                0
+            ]  # so far it will produce less recommendations for this customer
         all_groups = transactions.kmeans_consumer(customer_dummies, k=min_k)
         for i in range(min_k):
             one_group = all_groups.cluster_centers_[i]
             # We can use filtered dummies here
-            _, indices = self.model.nearest(row=one_group)    # same shape as filtered (with article_id)
+            _, indices = self.model.nearest(
+                row=one_group
+            )  # same shape as filtered (with article_id)
             for r in indices[0][: self.recomendations_by_group]:
                 article_id = self.filtered_dummies.iloc[r].article_id
                 recomendation_ids.append(article_id)
@@ -103,4 +111,3 @@ class KnnRecommender:
             df.loc[c] = {"prediction": " ".join(recommendations)}
         df = df.reset_index().rename(columns={"index": "customer_id"})
         return df
-
