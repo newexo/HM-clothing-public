38cafe511164d30a0789cc3614b38e8c86786a8a
Reuben Brasher <rkbrasher@gmail.com>
2023-08-28 16:21:25 -0700

    Generate random articles and random dates

--- hmcollab/tests/fake_data.py
+++ hmcollab/tests/fake_data.py
@@ -12,3 +12,35 @@ def fake_customers(n, r=None):
     ids = [customer_id(i) for i in range(n)]
     ages = r.randint(100, size=n)
     return pd.DataFrame.from_dict({"customer_id": ids, "age": ages})
+
+
+def articles_random_df(n=20, r=None):
+    if r is None:
+        r = np.random.RandomState(42)
+    article_id = ["0" + str(i) for i in range(n)]
+    color = ["black", "white"]
+    article_color = [color[r.randint(low=0, high=len(color))] for i in range(n)]
+    article_type = ["shirt", "pants", "shoes"]
+    article = [article_type[r.randint(low=0, high=len(article_type))] for i in range(n)]
+    return pd.DataFrame(
+        {"article_id": article_id, "color": article_color, "article": article}
+    )
+
+
+def dates_random(n_month=50, r=None):
+    if r is None:
+        r = np.random.RandomState(42)
+    years = [2021, 2022]
+    months = {"03": 31, "04": 30}
+    dates = []
+    for year in years:
+        for month, month_max in months.items():
+            days = r.randint(low=1, high=month_max, size=n_month)
+            temp = [
+                str(year) + "-" + str(month) + "-0" + str(day)
+                if day < 10
+                else str(year) + "-" + str(month) + "-" + str(day)
+                for day in days
+            ]
+            dates.extend(temp)
+    return dates
--- hmcollab/tests/test_fake_data.py
+++ hmcollab/tests/test_fake_data.py
@@ -26,3 +26,26 @@ class TestFakeData(unittest.TestCase):
         self.assertEqual(51, df.iloc[0].age)
         self.assertEqual(87, df.iloc[10].age)
         self.assertEqual(52, df.iloc[15].age)
+
+    def test_articles_random_df(self):
+        n = 10
+        df = fake_data.articles_random_df(n)
+        self.assertEqual(n, df.shape[0])
+        self.assertEqual(3, df.shape[1])
+        self.assertEqual("article_id", df.columns[0])
+        self.assertEqual("color", df.columns[1])
+        self.assertEqual("article", df.columns[2])
+        self.assertEqual("00", df.iloc[0].article_id)
+        self.assertEqual("09", df.iloc[9].article_id)
+        self.assertEqual("black", df.iloc[0].color)
+        self.assertEqual("white", df.iloc[5].color)
+        self.assertEqual("white", df.iloc[9].color)
+        self.assertEqual("shoes", df.iloc[0].article)
+        self.assertEqual("pants", df.iloc[9].article)
+
+    def test_dates_random(self):
+        n = 2
+        dates = fake_data.dates_random(n)
+        self.assertEqual(8, len(dates))
+        self.assertEqual("2021-03-07", dates[0])
+        self.assertEqual("2022-04-21", dates[-1])
