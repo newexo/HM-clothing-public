2e1cac630e48bdd79ee1ad221a15feadf6b9c3e3
Reuben Brasher <rkbrasher@gmail.com>
2023-09-05 21:18:32 -0700

    Replace test_model.py with integration tests using fivehundred dataset

--- hmcollab/tests/integration_tests/integration_test_models.py
+++ hmcollab/tests/integration_tests/integration_test_models.py
@@ -1,29 +1,23 @@
 import unittest
 import warnings
 
-import pandas as pd
-
 from hmcollab import articles
 from hmcollab import datasets
 from hmcollab import directories
 from hmcollab import models
-from hmcollab import scoring
 from hmcollab.directory_tree import HMDatasetDirectoryTree
 
 warnings.filterwarnings("ignore")
 
 
-# TODO: replace these tests with integration tests depending on data in testdata/fivehundred
-class TestModels(unittest.TestCase):
+class IntegrationTestModels(unittest.TestCase):
     def setUp(self):
-        self.tree = HMDatasetDirectoryTree(base=directories.testdata("ten_customers"))
+        self.tree = HMDatasetDirectoryTree(base=directories.testdata("fivehundred"))
         self.dataset = datasets.HMDataset(tree=self.tree)
         self.articles_munger = articles.ArticleFeaturesSimpleFeatures(
             self.dataset.articles, use_article_id=True
         )
-        self.customer = (
-            "0000757967448a6cb83efb3ea7a3fb9d418ac7adf2379d8cd0c725276a467a2a"
-        )
+        self.customer = self.dataset.customers.customer_id[10]
         self.clusters = 2
         self.days = 7
         self.full_dummies = articles.ArticleFeaturesSimpleFeatures(
@@ -31,8 +25,8 @@ class TestModels(unittest.TestCase):
         ).x
 
         self.customer_list = [
-            "000058a12d5b43e67d225668fa1f8d618c13dc232df0cad8ffe7ad4a1091e318",
-            "0000757967448a6cb83efb3ea7a3fb9d418ac7adf2379d8cd0c725276a467a2a",
+            self.customer,
+            self.dataset.customers.customer_id[100],
         ]
 
     def tearDown(self):
@@ -47,17 +41,10 @@ class TestModels(unittest.TestCase):
             threshold=0,
         )
         actual = recommender.recommend(self.customer)
-        expected = [
-            "0726925001",
-            "0735843004",
-            "0559630026",
-            "0715624008",
-            "0783388001",
-            "0377277001",
-        ]
+        expected_indices = [478, 1038, 1309, 427, 7409,  432]
+        expected = list(recommender.filtered_dummies.iloc[expected_indices].article_id)
         self.assertEqual(expected, actual)
 
-    # TODO: create new integration test based on this
     def test_recommend_all_drop_duplicates_with_threshold(self):
         recommender = models.KnnRecommender(
             self.dataset,
@@ -77,12 +64,15 @@ class TestModels(unittest.TestCase):
         self.assertEqual(expected, actual)
 
         actual = df.prediction[0]
-
-        expected = "0351484002 0723529001 0351484002 0723529001"
+        expected_indices = [201, 349, 106, 1172]
+        expected_list = list(recommender.filtered_dummies.iloc[expected_indices].article_id)
+        expected = " ".join(expected_list)
         self.assertEqual(expected, actual)
 
         actual = df.prediction[1]
-        expected = "0351484002 0723529001 0351484002 0723529001"
+        expected_indices = [163, 586, 722, 727]
+        expected_list = list(recommender.filtered_dummies.iloc[expected_indices].article_id)
+        expected = " ".join(expected_list)
         self.assertEqual(expected, actual)
 
     def test_popular_recommender(self):
@@ -94,20 +84,8 @@ class TestModels(unittest.TestCase):
 
         # 1. Test recommend
         actual_recommend = recommender.recommend()
-        expected = [
-            "0351484002",
-            "0723529001",
-            "0811835004",
-            "0689898002",
-            "0640174001",
-            "0797065001",
-            "0599580055",
-            "0811927004",
-            "0811925005",
-            "0800436010",
-            "0666448006",
-            "0663713001",
-        ]
+        expected_indices = [6721, 9356, 2150, 1720, 27, 6722, 9901, 13439, 5507, 2655, 489, 2148]
+        expected = list(self.dataset.articles.iloc[expected_indices].article_id)
 
         self.assertEqual(expected, actual_recommend)
 
@@ -124,35 +102,3 @@ class TestModels(unittest.TestCase):
         actual = actual_recommend_all.iloc[0, 1]
         expected = " ".join(actual_recommend)
         self.assertEqual(expected, actual)
-
-        y_by_customer = pd.read_csv(
-            directories.data(filename="target_set_7d_75481u.csv")
-        )
-
-        # originally, prediction failed for all. Fix so that one recommendation is correct to test ap_at_k and map_at_k
-        actual_recommend_all.prediction = actual_recommend_all.iloc[
-            0
-        ].prediction.replace("0811835004", "0794321007")
-        t = scoring.relevant(actual_recommend_all, y_by_customer)
-        self.assertEqual((1,), t.shape)
-        self.assertEqual((12,), t[0].shape)
-        self.assertEqual(
-            [
-                False,
-                False,
-                True,
-                False,
-                False,
-                False,
-                False,
-                False,
-                False,
-                False,
-                False,
-                False,
-            ],
-            list(t[0]),
-        )
-
-        self.assertAlmostEquals(0.027777777777777776, scoring.ap_at_k(t[0]))
-        self.assertAlmostEquals(0.027777777777777776, scoring.map_at_k(t))
