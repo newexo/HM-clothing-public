0ff1bc6bfa183680d79d0cc795a64a67928fc0d7
Reuben Brasher <rkbrasher@gmail.com>
2023-09-11 21:22:42 -0700

    Integration test for KnnRecommender_for3

--- hmcollab/tests/integration_tests/integration_test_models.py
+++ hmcollab/tests/integration_tests/integration_test_models.py
@@ -13,7 +13,7 @@ warnings.filterwarnings("ignore")
 class IntegrationTestModels(unittest.TestCase):
     def setUp(self):
         self.tree = HMDatasetDirectoryTree(base=directories.testdata("fivehundred"))
-        self.dataset = datasets.HMDataset(tree=self.tree)
+        self.dataset = datasets.HMDatasetTwoSets(tree=self.tree)
         self.articles_munger = articles.ArticleFeaturesSimpleFeatures(
             self.dataset.articles, use_article_id=True
         )
@@ -101,4 +101,65 @@ class IntegrationTestModels(unittest.TestCase):
 
         actual = actual_recommend_all.iloc[0, 1]
         expected = " ".join(actual_recommend)
+        self.assertEqual(expected, actual)
+
+    def test_knn_recommender_for3(self):
+        recommender = models.KnnRecommender_for3(
+            self.dataset,
+            self.full_dummies,
+            groups=2,
+            total_recommendations=6,
+            threshold=0,
+        )
+        actual = recommender.recommend(self.customer)
+        expected_indices = [478, 1038, 1309, 427, 7409,  432]
+        expected = list(recommender.filtered_dummies.iloc[expected_indices].article_id)
+        self.assertEqual(expected, actual)
+
+    def test_knn_recommender_for3_val(self):
+        dataset = datasets.HMDatasetThreeSets(threepartdataset=self.dataset)
+
+        val_x_customer_ids_set = set(dataset.val_x.customer_id)
+        customers_at_vy = val_x_customer_ids_set.intersection(
+            set(dataset.val_y.customer_id)
+        )
+        customers_at_vy = list(customers_at_vy)
+        customers_at_vy.sort()
+        customer = customers_at_vy[10]
+
+        recommender = models.KnnRecommender_for3(
+            dataset,
+            self.full_dummies,
+            groups=2,
+            total_recommendations=6,
+            threshold=0,
+            split='val'
+        )
+        actual = recommender.recommend(customer)
+        expected_indices = [5758, 8133, 1333, 8926, 15604, 16134]
+        expected = list(recommender.filtered_dummies.loc[expected_indices].article_id)
+        self.assertEqual(expected, actual)
+
+    def test_knn_recommender_for3_test(self):
+        dataset = datasets.HMDatasetThreeSets(threepartdataset=self.dataset)
+
+        test_x_customer_ids_set = set(dataset.test_x.customer_id)
+        customers_at_ty = test_x_customer_ids_set.intersection(
+            set(dataset.test_y.customer_id)
+        )
+        customers_at_ty = list(customers_at_ty)
+        customers_at_ty.sort()
+        customer = customers_at_ty[10]
+
+        recommender = models.KnnRecommender_for3(
+            dataset,
+            self.full_dummies,
+            groups=2,
+            total_recommendations=6,
+            threshold=0,
+            split='test'
+        )
+        actual = recommender.recommend(customer)
+        expected_indices = [10188, 4276, 796]
+        expected = list(recommender.filtered_dummies.loc[expected_indices].article_id)
         self.assertEqual(expected, actual)
